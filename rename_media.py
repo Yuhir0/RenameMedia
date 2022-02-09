fc1f0d9d1e3b6ce6a6d986fd23ff3dd8c5e18107
Óscar García <oscar.garcia@basetis.com>
2022-02-09 11:24:46 +0100

    Manage config file per title

--- rename_media.py
+++ rename_media.py
@@ -1,9 +1,14 @@
 import sys
+import subprocess
 import re
 import os
 
 from datetime import datetime
 
+subprocess.check_output([sys.executable, '-m', 'pip', 'install', '-r', '.requirements'])
+
+# Import requirements
+import yaml
 
 
 LOG_FILE = sys.modules[__name__].__file__.replace('.py', '.log')
@@ -32,6 +37,8 @@ args_dict = dict(
     title=''
 )
 
+conf_params = dict()
+
 
 def log(text):
     loging.write(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}]: {text}\n')
@@ -54,7 +61,13 @@ def get_season_from_path(path):
     search = re.search(season_pattern, path, flags=re.IGNORECASE)	
     if search:
         return search.group(1)
-    return '01'
+    return conf_params.get('season', '01')
+
+
+def get_season(path):
+    return str(conf_params.get('season', '01') if \
+        conf_params.get('force_confe', False) \
+        else get_season_from_path(path)).zfill(2)
 
 
 def store_chap_metadata(path, chap):
@@ -64,11 +77,14 @@ def store_chap_metadata(path, chap):
 
 def get_next_chapter(path):
     full_path = os.path.join(path, CHAPTER_METADATA)
+    chap = None
     if os.path.exists(full_path):
         with open(full_path, 'r') as metadata:
-            chap = str(int(metadata.read()) + 1)
-    else:
-        chap = '01'
+            meta = metadata.read()
+            if meta:
+                chap = str(int(meta) + 1)
+    if not chap:
+        chap = str(conf_params.get('first_cap', '01'))
     store_chap_metadata(path, chap)
     return chap
 
@@ -76,7 +92,7 @@ def get_next_chapter(path):
 def get_chap_and_ext_from_name(name, path):
     search = re.search(only_chapter_pattern, name)
     if search:
-        chap = search.group(1)
+        chap = get_next_chapter(path) if conf_params.get('sequencial', False) else search.group(1)
         ext = search.group(2)
         store_chap_metadata(path, chap)
         return chap, ext
@@ -84,15 +100,22 @@ def get_chap_and_ext_from_name(name, path):
         return get_next_chapter(path), '.mkv'
 
 
+def get_title():
+    return conf_params.get('title', '') if \
+         not args_dict['title'] or conf_params.get('force_confe', False) \
+         else args_dict['title']
+
+
 def replace_name(name, path):
     log(f'name -> {name}')
-    season = get_season_from_path(path)
+    season = get_season(path)
     log(f'season -> {season}')
     chap, ext = get_chap_and_ext_from_name(name, path)
     log(f'chap -> {chap}')
     new_name = name
     if args_dict["title"]:
-        new_name = f'{args_dict["title"]} S{season} E{chap.zfill(2)}{ext}'
+        title = get_title()
+        new_name = f'{title} S{season} E{chap.zfill(2)}{ext}'
     else:
         for pattern, replacement in REPLACEMENTS:
             new_name = re.sub(pattern, replacement, new_name)
@@ -102,6 +125,15 @@ def replace_name(name, path):
     full_path = os.path.join(path, name)
     new_full_path = os.path.join(path, new_name)
     os.rename(full_path, new_full_path)
+
+
+def load_title_config(path):
+    conf_path = os.path.join(path, '.title_conf.yml')
+    if os.path.exists(conf_path):
+        with open(conf_path) as ymlfile:
+            confp = yaml.load(ymlfile, yaml.loader.Loader)
+        conf_params.update(confp)
+        log(f'title conf -> {conf_params}')
 
 
 def dir_sort_filter(item):
@@ -122,6 +154,7 @@ def replace_all_files_in_path(path):
 def main(*args):
     get_args(*args)
     log(args_dict)
+    load_title_config(args_dict['path'])
     if args_dict['all'].lower() == 'yes':
         replace_all_files_in_path(args_dict['path'])
     else:
